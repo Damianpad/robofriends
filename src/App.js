afe592505e79bd829cb47d24c1f68c3904390b3e
Damian Padilla <damipad4@gmail.com>
2022-07-13 19:51:00 -0500

    Added React study from library.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +25,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
