9a2a41537500305124b6b36501232172c8eeb3b4
Damian Padilla <damipad4@gmail.com>
2022-07-14 22:55:21 -0500

    Added change from my PC

--- src/Hello.js
+++ src/Hello.js
@@ -4,10 +4,11 @@ import './Hello.css';
 class Hello extends Component {
   render() {
     return (
-    <div class='f1 tc'>
-    <h1>Hello World</h1>
-    <p>Welcome to react</p>
-    </div>
+      <div class='f1 tc'>
+        <h1>Hello World</h1>
+        <p>Welcome to react</p>
+        <p>This change was made on my PC</p>
+      </div>
     )
   }
 }
